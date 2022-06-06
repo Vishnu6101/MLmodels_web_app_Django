0c3f8a611b9536f176e6fe851c22129e96c6f2b1
Vishnukumar NV <vishnukumarnv6101@gmail.com>
2022-06-06 23:35:46 +0530

    allowed hosts

--- MLmodels/settings.py
+++ MLmodels/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lx0sjgwdcn*ev69t2oo1z=sqy@u+uu#-p)q-y)l%hgyxn8wuy-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ml-world-1240.herokuapp.com', 'localhost']
 
 
 # Application definition
